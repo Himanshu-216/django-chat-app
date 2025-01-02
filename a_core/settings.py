f9a9039c6adfdc281be7209d7889ba633f77fe20
harvey.himanshu <harvey.himanshu@rubicotech.in>
2025-01-02 14:55:03 +0530

    mysql db setup

--- a_core/settings.py
+++ a_core/settings.py
@@ -91,12 +91,7 @@ WSGI_APPLICATION = 'a_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+https://github.com/Himanshu-216/django-chat-app.git
 
 
 # Password validation
