628069561dea362bdd760f342108c1b0730fd644
Himanshu <himanshu162pnt@gmail.com>
2025-01-10 07:04:28 +0530

    chat message functionality

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect 
+from django.contrib.auth.decorators import login_required
+# from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from django.http import HttpResponse
+from django.contrib import messages
+from django.http import Http404
+from .models import *
+# from .forms import *
 
 
 def chat_view(request):
-    return render(request, 'a_rtchat/chat.html')
+    chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+    return render(request, 'a_rtchat/chat.html', {'chat_messages' : chat_messages})
